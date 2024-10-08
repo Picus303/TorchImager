38aa38f73b4accd4dc0d03cce73864763e9001ab
Picus303 <pilleurc@protonmail.com>
2024-10-08 06:07:48 +0200

    fix install process for PyPI

--- setup.py
+++ setup.py
@@ -4,6 +4,19 @@ import shutil
 import platform
 from skbuild import setup
 from setuptools import find_packages
+from setuptools.command.install import install
+
+class CleanInstall(install):
+    def run(self):
+        install.run(self)
+        
+        install_path = self.install_lib
+        extensions = [".cpp", ".h", ".cu", ".hip"]
+
+        for root, dirs, files in os.walk(install_path):
+            for file in files:
+                if any(file.endswith(ext) for ext in extensions):
+                    os.remove(os.path.join(root, file))
 
 hipcc_path = shutil.which("hipcc")
 if hipcc_path is None:
@@ -23,10 +36,13 @@ setup(
     url="https://github.com/Picus303/TorchImager",
     license="MIT",
     packages=find_packages(),
+    cmake_install_dir="TorchImager",
     cmake_minimum_required_version="3.5",
     cmake_args=[
         f"-DPYTHON_EXECUTABLE={sys.executable}"
     ],
+    include_package_data=True,
     python_requires=">=3.12",
     install_requires=["torch"],
+    cmdclass={"install": CleanInstall}
 )
